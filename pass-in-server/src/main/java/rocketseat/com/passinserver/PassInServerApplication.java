1406100d32e9a96d23378913849143738a9c4957
Vinícius Lima <villylss@gmail.com>
2024-04-02 23:18:31 -0300

    init: projeto pass-in java iniciado

---
+++ pass-in-server/src/main/java/rocketseat/com/passinserver/PassInServerApplication.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInServerApplication.class, args);
+	}
+
+}
---
+++ pass-in-server/src/test/java/rocketseat/com/passinserver/PassInServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rocketseat.com.passinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
